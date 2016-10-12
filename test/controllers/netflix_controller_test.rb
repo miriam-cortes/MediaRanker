347d9c58242761203cb6094145300476a1c3bd45
Miriam Cortes <mcortes.umiami@gmail.com>
2016-10-12 16:23:14 -0700

    all but one test passed for netflix, I just feel like I should push

--- test/controllers/netflix_controller_test.rb
+++ test/controllers/netflix_controller_test.rb
@@ -4,20 +4,11 @@ class NetflixControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_template :index
   end
 
   test "should get show" do
-    get :show
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit
-    assert_response :success
-  end
-
-  test "should get update" do
-    get :update
+    get :show, {id: netflixes(:movieone).id }
     assert_response :success
   end
 
@@ -26,14 +17,39 @@ class NetflixControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get create" do
-    get :create
+  test "should get edit" do
+    get :edit, {id: netflixes(:movieone).id}
     assert_response :success
   end
 
-  test "should get destroy" do
-    get :destroy
+  test "should be able to create" do
+    post_params = { netflix: {name: "movie3", description: "a movie suggestion", genre: "comedy"} }
+    post :create, post_params
+    assert_response :redirect
+  end
+
+  test "creating a new show changes the number of shows" do
+    assert_difference("Netflix.count", 1) do
+      post_params = { netflix: {name: "another movie", description: "a movie suggestion", genre: "comedy"} }
+      post :create, post_params
+    end
+  end
+
+  test "should be able to update a movie" do
+    patch :update, { id: netflixes(:movietwo).id }
     assert_response :success
+  end
+
+  test "should be able to delete" do
+    post_params = { netflix: {name: "netflix1"} }
+    delete :destroy, {id: netflixes(:movieone).id}
+    assert_response :redirect
+  end
+
+  test "should be able to delete movieone" do
+    assert_difference("Netflix.count", -1) do
+      delete :destroy, { id: netflixes(:movieone).id }
+    end
   end
 
 end
